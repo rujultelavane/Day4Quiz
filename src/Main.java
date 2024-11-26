12f64b062596550b29cf590a64a3e0bd2e1ef455
suha-0116 <suham601@gmail.com>
2024-11-25 22:02:33 -0500

    Instance of Interface
    Created an instance of the interface and wrote the method using a lambda expression.

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
 public class Main {
+
+    public static void main (String[] args)
+    {
+        Addition a = (int x, int y) -> {
+            int sum = 0;
+            for (int i = x; i < y+1; i ++ )
+            {
+                sum += i;
+            }
+            return sum;
+        };
+    }
 }
