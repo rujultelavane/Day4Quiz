501415c4aba0832fcf2fc688a7d6b20a7ed7f136
suha-0116 <suham601@gmail.com>
2024-11-27 11:25:19 -0500

    Command Line Arguments
    Used command line arguments to make sure code works

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,27 @@ public class Main {
 
     public static void main (String[] args)
     {
-        Addition a = (int x, int y) -> {
-            int sum = 0;
-            for (int i = x; i < y+1; i ++ )
-            {
-                sum += i;
-            }
-            return sum;
-        };
+        if (args.length == 2){
+            int s = Integer.parseInt(args[0]);
+            int e = Integer.parseInt(args[1]);
+
+            Addition a = (int x, int y) -> {
+                int sum = 0;
+                for (int i = x; i < y+1; i ++ )
+                {
+                    sum += i;
+                }
+                return sum;
+            };
+
+            int total = a.add(s, e);
+
+            System.out.print(total);
+
+        }
+
     }
+
+
+
 }
